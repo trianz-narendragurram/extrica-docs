83d3647454001a476c346b9c9338dd22c3517b63
narendra gurram <narendra.gurram@trianz.com>
2024-06-26 15:59:39 +0530

    code fix

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/narendra/MyWorkspace/extrica-docs-poc/extrica-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/narendra/MyWorkspace/extrica-docs-poc/extrica-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/narendra/MyWorkspace/extrica-docs-poc/extrica-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/narendra/MyWorkspace/extrica-docs-poc/extrica-docs/src/css/custom.css"),
+];
