f61cd7026186cd7dfe9859173a6f30288b68ebea
narendra gurram <narendra.gurram@trianz.com>
2024-07-04 11:48:27 +0530

    search added

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -128,6 +128,7 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+  plugins: [require.resolve('docusaurus-lunr-search')],
 };
 
 export default config;
